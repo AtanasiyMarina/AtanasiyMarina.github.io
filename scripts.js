718b900493ed61ae7f466ded1d157fe0565f59cc
AtanasiyMarina <m.atanasiy@gmail.com>
2021-07-02 17:56:41 +0300

    Update scripts.js
    Updated scripts

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 'use strict'
 let burgerMenu = document.querySelector('.burger-menu');
 let navItems = document.querySelector('.nav-items');
-
+let sendButton = document.querySelector('.send-button');
 burgerMenu.addEventListener('click',event => {
     event.preventDefault();
     navItems.classList.toggle('active');
     burgerMenu.classList.toggle('active');
+})
+
+sendButton.addEventListener('click',event =>{
+    event.preventDefault();
 })
